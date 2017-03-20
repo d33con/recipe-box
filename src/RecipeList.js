96477f8d833d2a979cc647c3179e9d86345ff281
Oli Bullen <d33con@gmail.com>
2017-03-20 18:25:29 +0000

    initial commit

--- src/RecipeList.js
+++ src/RecipeList.js
@@ -1,43 +1,80 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Card, List, Button, Icon } from 'semantic-ui-react';
 
-import recipes from './defaultRecipes';
+import recipes from './data/defaultRecipes';
 
-const RecipeList = () => {
-    return (
-        <div>
-            <Card.Group>
-                {recipes.map(recipe => {
-                    return (
-                        <Card key={recipe.id}>
-                            <Card.Content>
-                                <Card.Header>{recipe.title}</Card.Header>
-                                <Card.Description>
-                                    <List>
-                                        {recipe.ingredients.map((ingredient, i) => <List.Item key={i}>{` - ${ingredient}`}</List.Item>)}
-                                    </List>
-                                </Card.Description>
-                            </Card.Content>
-                            <Card.Content extra>
-                                <Button animated="vertical" color='yellow'>
-                                    <Button.Content visible>Favourite</Button.Content>
-                                    <Button.Content hidden><Icon name='star' /></Button.Content>                                            
-                                </Button>
-                                <Button animated="vertical" color='green'>
-                                    <Button.Content visible>Edit</Button.Content>
-                                    <Button.Content hidden><Icon name='pencil' /></Button.Content>                                            
-                                </Button>
-                                <Button animated="vertical" color='red'>
-                                    <Button.Content visible>Delete</Button.Content>
-                                    <Button.Content hidden><Icon name='delete' /></Button.Content>                                            
-                                </Button>
-                            </Card.Content>
-                        </Card>
+class RecipeList extends Component {
+    constructor() {
+        super();
+        this.state = {
+            recipes
+        }
+    }
+
+    deleteRecipe = (id) => {
+        const newRecipes = [
+            ...this.state.recipes.slice(0, id),
+            ...this.state.recipes.slice(id+1)
+        ];
+        this.setState({
+            recipes: newRecipes
+        });
+    }
+
+    handleClick = (id) => {
+        if(this.state.recipes.id !== id) {
+            this.setState({
+                recipes
+            }); 
+        }
+
+        this.setState({
+            ...this.state.recipes,
+            isFavourite: !this.state.recipes.isFavourite
+        }) 
+    }
+
+    render() {        
+        return (
+            <div>
+                <Card.Group>
+                    {recipes.map(recipe => {
+                        return (
+                            <Card key={recipe.id}>
+                                <Card.Content>
+                                    <Card.Header>{recipe.title}</Card.Header>
+                                    <Card.Description>
+                                        <List>
+                                            {recipe.ingredients.map((ingredient, i) => <List.Item key={i}>{` - ${ingredient}`}</List.Item>)}
+                                        </List>
+                                    </Card.Description>
+                                </Card.Content>
+                                <Card.Content extra>                                
+                                    {(recipe.isFavourite) ? <Icon 
+                                                                name='star' 
+                                                                color='yellow' 
+                                                                size='large' 
+                                                                onClick={this.handleClick.bind(null, recipe.id)}/> 
+                                                        : <Icon 
+                                                            name='empty star' 
+                                                            color='grey' 
+                                                            size='large' />}                                           
+                                    <Button basic animated="vertical" color='green'>
+                                        <Button.Content visible>Edit</Button.Content>
+                                        <Button.Content hidden><Icon name='pencil' /></Button.Content>                                            
+                                    </Button>
+                                    <Button basic animated="vertical" color='red' onClick={this.deleteRecipe.bind(null, recipe.id)}>
+                                        <Button.Content visible>Delete</Button.Content>
+                                        <Button.Content hidden><Icon name='delete' /></Button.Content>                                            
+                                    </Button>
+                                </Card.Content>
+                            </Card>
+                        )}
                     )}
-                )}
-            </Card.Group>
-        </div>
-    );
+                </Card.Group>
+            </div>
+        );
+    }
 };
 
 RecipeList.propTypes = {
