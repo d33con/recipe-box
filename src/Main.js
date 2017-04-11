0c31ed722f0348f94548a2d60692937ffe4162e8
Oli Bullen <d33con@gmail.com>
2017-04-11 22:25:11 +0900

    implement routing

--- src/Main.js
+++ src/Main.js
@@ -1,15 +1,11 @@
 import React from 'react';
 
-import RecipeList from './RecipeList';
-import AddRecipe from './AddRecipe';
 import UserMenu from './UserMenu';
 
 const Main = () => {
     return (
       <div>
           <UserMenu />
-          <RecipeList />
-          <AddRecipe />
       </div>
     )
 };
--- src/Root.js
+++ src/Root.js
@@ -1,12 +1,23 @@
 import React, { PropTypes } from 'react';
 import { Provider } from 'react-redux';
-import Main from './Main';
 import { BrowserRouter as Router, Route, browserHistory } from 'react-router-dom';
+
+import Main from './Main';
+import Home from './Home';
+import RecipeList from './RecipeList';
+import Favourites from './Favourites';
+import AddRecipe from './AddRecipe';
 
 const Root = ({ store }) => (
     <Provider store={store}>
         <Router history={browserHistory}>
-            <Route path="/" component={Main} />
+            <div>
+                <Route path="/" component={Main} />
+                <Route exact path="/" component={Home} />
+                <Route path="/recipes" component={RecipeList} />
+                <Route path="/favourites" component={Favourites} />
+                <Route path="/addrecipe" component={AddRecipe} />
+            </div>
         </Router>
     </Provider>
 );
